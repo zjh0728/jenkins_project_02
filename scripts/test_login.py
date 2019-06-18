a114e6a5e283a59e93b5ddb8d734f5cd3343210c
zjh0728 <13381466007@163.com>
2019-06-18 16:00:52 +0800

    first commit

---
+++ scripts/test_login.py
@@ -0,0 +1,13 @@
+class TestLogin:
+
+    def test_login1(self):
+        assert 1
+
+    def test_login2(self):
+        assert 1
+
+    def test_login3(self):
+        assert 0
+
+    def test_login4(self):
+        assert 1
